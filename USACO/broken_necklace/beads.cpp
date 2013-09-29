2125c6fb9715303515fe6843a4c185c0c74e3f5e
Pardeep Singh <singh.pardeep6605@gmail.com>
2013-09-28 20:38:48 -0700

    Broken necklace 2013

--- USACO/broken_necklace/beads.cpp
+++ USACO/broken_necklace/beads.cpp
@@ -1,64 +1,84 @@
 /*
 ID: singh.p2
-PROG: beads
 LANG: C++
+TASK: beads
 */
+#include <fstream>
+#include <iostream>
 #include <cstdio>
 #include <cstring>
+
 using namespace std;
 
-int sum(void);
-char s[351]={0},st[351]={0};
-int n=0;
-int main(void)
-{
-    int i=0,j=0,max=-1,t=0;
+int max_beads(char *necklace);
 
-    freopen("beads.in","rt",stdin);
-    scanf("%d",&n);
-    scanf("%s",&s);
-    fclose(stdin);
-    for (i=0;i<=n-1;i++)
-    {
-        for (j=i+1;j<=n-1;j++) st[j-i-1]=s[j];
-        for (j=i;j>=0;j--) st[j+n-i-1]=s[j];
-        t=sum();
-        if (max<t) max=t;
-        t=0;
-        memset(st,'\0',sizeof(st));
+int n = 0;
+
+int main() {
+    ofstream fout ("beads.out");
+    ifstream fin ("beads.in");
+
+    string necklace;
+
+    fin >> n;
+    fin >> necklace;
+
+    char broken_necklace[n];
+    int max = -1;
+
+    for (int i = 0; i <= n-1; ++i) {
+        int j_max = 0;
+        for (int j = i+1; j <= n-1; ++j, j_max = j-i-1) {
+            broken_necklace[j-i-1] = necklace[j]; // j-i-1 to start at index 0
+        }
+
+        for (int j = 0; j <= i; ++j, ++j_max) {
+            broken_necklace[j_max] = necklace[j];
+        }
+
+        int total = max_beads(broken_necklace);
+        if (total > max) max = total;
     }
-    if (max==2*n) max=max/2;
-    freopen("beads.out","wt",stdout);
-    printf("%d\n",max);
-    fclose(stdout);
+
+    if(max == 2*n) max /= 2;
+    fout << max << endl;
     return 0;
 }
 
-int sum(void)
-{
-    int i=0,k=0;
-    char flag={0};
-    if (st[0]=='r') flag='r';
-    else if (st[0]=='b') flag='b';
-    else if (st[0]=='w')
-    {
-        while (st[i]=='w') i=i+1;
-        if (i==n) flag='w';
-        else flag=st[i];
+int max_beads(char *necklace) {
+
+    // count the left side
+    char flag = necklace[0];
+    int i = 0;
+    if(necklace[0] == 'w') {
+        while(necklace[i] == 'w') ++i;
+
+        // if we went through entire string and still seeing 'w'
+        if (i == n) flag = 'w';
+        else flag = necklace[i];
     }
-    i=0;
-    while ((st[i]==flag)||(st[i]=='w')) {k=k+1;st[i]=flag;i=i+1;}
-    flag='\0';
-    if (st[n-1]=='r') flag='r';
-    else if (st[n-1]=='b') flag='b';
-    else if (st[n-1]=='w')
-    {
-        i=n-1;
-        while (st[i]=='w') i=i-1;
-        if (i==-1) flag='w';
-        else flag=st[i];
+
+    int counter = 0;
+    i = 0;
+    while(flag == necklace[i] || necklace[i] == 'w') {
+        ++counter;
+        necklace[i] = flag;
+        ++i;
     }
-    i=n-1;
-    while ((st[i]==flag)||(st[i]=='w')) {k=k+1;st[i]=flag;i=i-1;}
-    return k;
+
+    // count the right side
+    i = n-1;
+    flag = necklace[n-1];
+    if (necklace[n-1] == 'w') {
+        while(necklace[i] == 'w') --i;
+        if (i < 0) flag = 'w';
+        else flag = necklace[i];
+    }
+
+    i = n-1;
+    while(flag == necklace[i] || necklace[i] == 'w') {
+        ++counter;
+        --i;
+    }
+    return counter;
 }
